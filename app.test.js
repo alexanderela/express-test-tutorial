9e2db6687ee90a80bf33ed687e5b81bf37fdfa1a
Alexander Ela <ela.alexander@gmail.com>
2019-04-27 15:46:56 -0600

    Add describe and IT blocks to app.test.js

--- app.test.js
+++ app.test.js
@@ -4,6 +4,7 @@ const database = require('knex')(configuration)
 
 import request from 'supertest'
 import app from './app'
+import students from './students'
 
 
 describe('Server', () => {
@@ -12,5 +13,29 @@ describe('Server', () => {
       const res = await request(app).get('/')
       expect(res.status).toBe(200)
     })
+  })
+
+  describe('', () => {
+  	it('', () => {})
+  })
+
+  describe('', () => {
+  	it('', () => {})
+  })
+
+  describe('', () => {
+  	it('', () => {})
+  })
+
+  describe('', () => {
+  	it('', () => {})
+  })
+
+  describe('', () => {
+  	it('', () => {})
+  })
+
+  describe('', () => {
+  	it('', () => {})
   })
 })
